0ca09308b941430be08fb9566129fa68fa5e586a
vikas7312sharma <vikas.sharma@infinitelocus.com>
2026-07-11 19:01:55 +0530

    add some more topics

---
+++ Practice/basic/server.js
@@ -0,0 +1,30 @@
+const express =  require('express')
+const cors = require('cors')
+const axios = requir('axio')
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors({
+    origin: '*'
+}));
+
+app.get('/health', ()=>{
+    console.log('Health Ok');
+})
+
+app.get('/getUsers', async (req, res) => {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');        
+        const result = response.data;
+        res.status(200).json({ "message": true, "data": result });
+    } catch (err) { 
+        console.log('Error: ', err.message);
+        res.status(400).json({ "message": false, "data": null });
+    }
+});
+
+app.listen(PORT, ()=>{
+    console.log('Server is running on PORT: ', PORT);
+})
